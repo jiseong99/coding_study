f78d5047006a032a43a176382440e1769adc393c
jiseong99 <137580822+jiseong99@users.noreply.github.com>
2023-09-01 17:37:50 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,17 @@
+'''
+def solution(players, callings):
+        for calls in callings:
+            a = players.index(calls)
+            c = players[a-1]
+            players[a-1] = calls
+            players[a] = c
+        return players
+'''
+def solution(players, callings):
+    result = {player: i for i, player in enumerate(players)} # 선수: 등수
+    for who in callings:
+        idx = result[who] # 호명된 선수의 현재 등수
+        result[who] -= 1 # 하나 앞 등수로 바꿔줌 -1
+        result[players[idx-1]] += 1 # 앞에 위치했던 선수의 등수 +1
+        players[idx-1], players[idx] = players[idx], players[idx-1] # 위치 변경
+    return players
