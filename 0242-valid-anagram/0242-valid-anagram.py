423c3210ef4ac286a19cf066b0b59efc086ecdaf
Ji-Seong Han <137580822+jiseong99@users.noreply.github.com>
2026-09-08 22:15:56 -0700

    Time: 11 ms (77.28%), Space: 19.4 MB (76.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+
+        for c in t:
+            if c not in count:
+                return False
+
+            count[c] -= 1
+
+            if count[c] < 0:
+                return False
+
+        return True
+
+
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+
+        return sorted_s == sorted_t
+'''
